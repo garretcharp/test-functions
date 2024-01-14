2c383b72d2a19239e4071f17d1ca38f759e44b74
Garret Harp <32212214+garretcharp@users.noreply.github.com>
2024-01-14 14:55:44 -0600

    Update test.ts

--- functions/test.ts
+++ functions/test.ts
@@ -1,5 +1,5 @@
 export async function onRequest(context) {
-  return new Response(JSON.stringify(context.request.cf), {
+  return new Response(JSON.stringify(context.request), {
       headers: {
         "content-type": "application/json;charset=UTF-8"
       }
